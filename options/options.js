87d97b768f4e7245fb190b822e8b63b48b4acbe9
Crowdbotics <team@crowdbotics.com>
2024-03-19 19:07:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-mouse-47140",
+  url: "https://withered-mouse-47140.botics.co",
+  api: "https://withered-mouse-47140.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
